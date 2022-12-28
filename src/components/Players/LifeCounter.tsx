d44a7fe21c09b6655be8ec51c60933ed1ad775db
alec <ajcuccia@gmail.com>
2022-12-28 16:08:28 -0500

    Responsive life counter borders

--- src/components/Players/LifeCounter.tsx
+++ src/components/Players/LifeCounter.tsx
@@ -12,8 +12,30 @@ const StyledLifeCounter = styled.li<{ flipped?: boolean, numPlayers: number, ord
   order: ${({ order }) => order};
   font-family: 'Noto Sans Mono', monospace;
   user-select: none;
-  border: 6px solid #000000;
-  border-radius: 24px;
+  /* border: 6px solid #000000; */
+  border: ${({ numPlayers }) => {
+    if (2 <= numPlayers && numPlayers <= 4) { return '6px solid #000000'; }
+    if (5 <= numPlayers && numPlayers <= 7) { return '4px solid #000000'; }
+    if (8 <= numPlayers && numPlayers <= 9) { return '2px solid #000000'; }
+    // if (numPlayers === 4) { return '50px'; }
+    // if (numPlayers === 5) { return '40px'; }
+    // if (numPlayers === 6) { return '35px'; }
+    // if (numPlayers === 7) { return '24px'; }
+    // if (numPlayers === 8) { return '18px'; }
+    // if (numPlayers === 9) { return '18px'; }
+  }};
+  /* border-radius: 24px; */
+  border-radius: ${({ numPlayers }) => {
+    if (2 <= numPlayers && numPlayers <= 4) { return '24px'; }
+    if (5 <= numPlayers && numPlayers <= 7) { return '20px'; }
+    if (8 <= numPlayers && numPlayers <= 9) { return '18px'; }
+    // if (numPlayers === 4) { return '50px'; }
+    // if (numPlayers === 5) { return '40px'; }
+    // if (numPlayers === 6) { return '35px'; }
+    // if (numPlayers === 7) { return '24px'; }
+    // if (numPlayers === 8) { return '18px'; }
+    // if (numPlayers === 9) { return '18px'; }
+  }};
   ${({ flipped }) => flipped && 'rotate: 180deg;'};
 
   .player-info {
